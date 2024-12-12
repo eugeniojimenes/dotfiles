68ab45613ff5acf688c53423a550ef1813520a1a
Eugenio Augusto Jimenes <eugeniojimenes@gmail.com>
2024-12-12 10:12:10 -0300

    feat: conditional wsl clipboard

--- lazyvim/lua/config/options.lua
+++ lazyvim/lua/config/options.lua
@@ -2,21 +2,28 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+local function is_wsl()
+  local output = vim.fn.system("uname -r")
+  return string.find(output, "WSL") ~= nil
+end
+
 vim.opt.wrap = true
 vim.opt.clipboard = "unnamedplus"
 vim.opt.colorcolumn = "120"
--- vim.g.clipboard = {
---   name = 'WslClipboard',
---   copy = {
---     ["+"] = "clip.exe",
---     ["*"] = "clip.exe",
---   },
---   paste = {
---     ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
---     ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
---   },
---   cache_enabled = 0,
--- }
+if is_wsl() then
+  vim.g.clipboard = {
+    name = 'WslClipboard',
+    copy = {
+      ["+"] = "clip.exe",
+      ["*"] = "clip.exe",
+    },
+    paste = {
+      ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0,
+  }
+end
 vim.opt.list = true
 vim.opt.listchars = { eol = "󱞥", trail = "", tab = ">-", nbsp = "~" }
 vim.opt.relativenumber = true
