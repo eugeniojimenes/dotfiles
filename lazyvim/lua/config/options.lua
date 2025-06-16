b6af7f2ffa37643cf227f7206d3d80e874d693d5
Eugenio Augusto Jimenes <eugeniojimenes@gmail.com>
2025-06-16 19:18:00 -0300

    chore: update configuration files and dependencies
    - Updated Hyprland monitor configuration.
    
    - Added new extras to LazyVim configuration including AI Copilot and Markdown support.
    
    - Commented out unused fold settings and ltex-ls language commands in LazyVim options.
    
    - Refactored Avante plugin configuration with new selectors and removed obsolete providers.
    
    - Updated dependencies in lazy-lock.json with new commits and removed unused plugins.
    
    Signed-off-by: Eugenio Augusto Jimenes <eugeniojimenes@gmail.com>

--- lazyvim/lua/config/options.lua
+++ lazyvim/lua/config/options.lua
@@ -45,57 +45,57 @@ vim.g.lazyvim_ruby_formatter = "rubocop"
 -- vim.g.lazyvim_ruby_formatter = "standardrb"
 -- end
 
--- -- Fold settings
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-vim.opt.foldtext = "v:lua.vim.treesitter.foldtext()"
+---- Fold settings
+-- vim.opt.foldmethod = "expr"
+-- vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+-- vim.opt.foldtext = "v:lua.vim.treesitter.foldtext()"
+--
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = "ruby",
+--   callback = function()
+--     vim.opt_local.foldmethod = "expr"
+--     vim.opt_local.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+--
+--     -- Custom fold text to show line count
+--     vim.opt_local.foldtext = [[substitute(getline(v:foldstart),'\t',repeat('\ ',&tabstop),'g').'  '.trim(getline(v:foldend)).' ('.(v:foldend-v:foldstart-1).' lines)']]
+--
+--     -- Configure folding ranges
+--     vim.treesitter.query.set(
+--       "ruby",
+--       "folds",
+--       [[
+--         ((method) @fold)
+--         ((class) @fold)
+--         ((module) @fold)
+--         ((block) @fold (#offset! @fold 0 0 0 -1))
+--         ((do_block) @fold (#offset! @fold 0 0 0 -1))
+--         ((if) @fold (#offset! @fold 0 0 0 -1))
+--         ((begin) @fold (#offset! @fold 0 0 0 -1))
+--         ((case) @fold (#offset! @fold 0 0 0 -1))
+--         ((rescue) @fold (#offset! @fold 0 0 0 -1))
+--         ((array) @fold (#offset! @fold 0 0 0 -1))
+--         ((hash) @fold (#offset! @fold 0 0 0 -1))
+--       ]]
+--     )
+--   end,
+-- })
 
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "ruby",
-  callback = function()
-    vim.opt_local.foldmethod = "expr"
-    vim.opt_local.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-
-    -- Custom fold text to show line count
-    vim.opt_local.foldtext = [[substitute(getline(v:foldstart),'\t',repeat('\ ',&tabstop),'g').'  '.trim(getline(v:foldend)).' ('.(v:foldend-v:foldstart-1).' lines)']]
-
-    -- Configure folding ranges
-    vim.treesitter.query.set(
-      "ruby",
-      "folds",
-      [[
-        ((method) @fold)
-        ((class) @fold)
-        ((module) @fold)
-        ((block) @fold (#offset! @fold 0 0 0 -1))
-        ((do_block) @fold (#offset! @fold 0 0 0 -1))
-        ((if) @fold (#offset! @fold 0 0 0 -1))
-        ((begin) @fold (#offset! @fold 0 0 0 -1))
-        ((case) @fold (#offset! @fold 0 0 0 -1))
-        ((rescue) @fold (#offset! @fold 0 0 0 -1))
-        ((array) @fold (#offset! @fold 0 0 0 -1))
-        ((hash) @fold (#offset! @fold 0 0 0 -1))
-      ]]
-    )
-  end,
-})
-
--- Set ltex-ls language on the fly
-vim.api.nvim_create_user_command("LtexLang", function(lang)
-  local language = lang.args
-  local clients = vim.lsp.get_active_clients()
-
-  for _, client in ipairs(clients) do
-    if client.name == "ltex" then
-      vim.api.nvim_notify(
-        "Set ltex-ls lang to " .. language,
-        vim.log.levels.INFO,
-        { title = "utils.functions", timeout = 2000 }
-      )
-      client.config.settings.ltex.language = language
-      vim.lsp.buf_notify(0, "workspace/didChangeConfiguration", { settings = client.config.settings })
-      return
-    end
-  end
-  vim.api.nvim_notify("ltex-ls isn't installed", vim.log.levels.ERROR, { title = "utils.functions", timeout = 2000 })
-end, { nargs = 1, desc = "Set ltex-ls language" })
+---- Set ltex-ls language on the fly
+-- vim.api.nvim_create_user_command("LtexLang", function(lang)
+--   local language = lang.args
+--   local clients = vim.lsp.get_active_clients()
+--
+--   for _, client in ipairs(clients) do
+--     if client.name == "ltex" then
+--       vim.api.nvim_notify(
+--         "Set ltex-ls lang to " .. language,
+--         vim.log.levels.INFO,
+--         { title = "utils.functions", timeout = 2000 }
+--       )
+--       client.config.settings.ltex.language = language
+--       vim.lsp.buf_notify(0, "workspace/didChangeConfiguration", { settings = client.config.settings })
+--       return
+--     end
+--   end
+--   vim.api.nvim_notify("ltex-ls isn't installed", vim.log.levels.ERROR, { title = "utils.functions", timeout = 2000 })
+-- end, { nargs = 1, desc = "Set ltex-ls language" })
