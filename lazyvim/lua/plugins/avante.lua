63ba00d41c1f1e61a20120f3dd1d51a3e1938e1f
Eugenio Augusto Jimenes <eugeniojimenes@gmail.com>
2025-06-16 19:18:00 -0300

    chore: update configuration files and dependencies
    - Updated Hyprland monitor configuration.
    
    - Added new extras to LazyVim configuration including AI Copilot and Markdown support.
    
    - Commented out unused fold settings and ltex-ls language commands in LazyVim options.
    
    - Refactored Avante plugin configuration with new selectors and removed obsolete providers.
    
    - Updated dependencies in lazy-lock.json with new commits and removed unused plugins.
    
    Signed-off-by: Eugenio Augusto Jimenes <eugeniojimenes@gmail.com>

--- lazyvim/lua/plugins/avante.lua
+++ lazyvim/lua/plugins/avante.lua
@@ -3,6 +3,11 @@ return {
   event = "VeryLazy",
   version = false, -- Never set this value to "*"! Never!
   opts = {
+      selector = {
+        provider = "snacks",
+        -- Options override for custom providers
+        provider_opts = {},
+      },
     sources = {
       -- Add 'avante' to the list
       default = { "avante", "lsp", "path", "luasnip", "buffer" },
@@ -19,36 +24,6 @@ return {
     -- add any opts here
     -- for example
     provider = "copilot",
-    providers = {
-      openai = {
-        endpoint = "https://api.openai.com/v1",
-        model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
-        extra_request_body = {
-          timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
-          temperature = 0.75,
-          max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-          --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
-        },
-      },
-      deepseek = {
-        __inherited_from = "openai",
-        api_key_name = "DEEPSEEK_API_KEY",
-        endpoint = "https://api.deepseek.com",
-        model = "deepseek-chat",
-      },
-      gemini = {
-        api_key_name = "GEMINI_API_KEY",
-        endpoint = "https://generativelanguage.googleapis.com/v1beta/models",
-        model = "gemini-2.0-flash",
-        -- model = "gemini-2.5-pro-preview-06-05",
-        timeout = 30000, -- Timeout in milliseconds
-        extra_request_body = {
-          generationConfig = {
-            temperature = 0.75,
-          },
-        },
-      },
-    },
     behaviour = {
       auto_suggestions = false, -- Experimental stage
       auto_set_highlight_group = true,
@@ -71,11 +46,11 @@ return {
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
     --- The below dependencies are optional,
-    "echasnovski/mini.pick", -- for file_selector provider mini.pick
-    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+    -- "echasnovski/mini.pick", -- for file_selector provider mini.pick
+    -- "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
     "Kaiser-Yang/blink-cmp-avante", -- replament for "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-    "ibhagwan/fzf-lua", -- for file_selector provider fzf
-    "stevearc/dressing.nvim", -- for input provider dressing
+    -- "ibhagwan/fzf-lua", -- for file_selector provider fzf
+    -- "stevearc/dressing.nvim", -- for input provider dressing
     "folke/snacks.nvim", -- for input provider snacks
     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
     "zbirenbaum/copilot.lua", -- for providers='copilot'
