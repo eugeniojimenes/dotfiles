edf005753bebf6d5531c8558e83956ca4c263764
Eugenio Augusto Jimenes <eugeniojimenes@gmail.com>
2025-06-19 23:10:16 -0300

    feat(scratch-nvim): some updates
    - Added/updated plugins: codecompanion.nvim, copilot.lua, gitsigns.nvim,
    markview.nvim, nvim-ufo, promise-async, todo-comments.nvim
    - Refactored keymaps to use `snacks` module (lowercase) instead of
    `Snacks`
    - Improved Lua LSP config: removed verbose comments, streamlined setup
    - Switched vim options to use `vim.opt` and enabled undofile, increased
    undolevels
    - Added gitsigns.nvim and mini.nvim plugin setups
    - Enhanced tmux.conf: enabled focus-events, RGB terminal features, and
    set status line position to top

--- scratch-nvim/lua/plugins/utils.lua
+++ scratch-nvim/lua/plugins/utils.lua
@@ -1,12 +1,43 @@
 return {
   {
-    "folke/todo-comments.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {
-    },
+    "lewis6991/gitsigns.nvim",
+    opts = {}
   },
   {
     "echasnovski/mini.nvim",
+    event = "VeryLazy",
     version = "*",
+    config = function()
+      require('mini.comment').setup()
+      require('mini.sessions').setup()
+      local diff = require("mini.diff")
+      diff.setup({
+        -- Disabled by default
+        source = diff.gen_source.none(),
+      })
+      -- TODO: Serach more about it!
+      require('mini.git').setup() -- tpope/vim-fugitive alternative!!!
+    end,
   },
+  -- {
+  --   ---- Enable mini.diff for lualine
+  --   "nvim-lualine/lualine.nvim",
+  --   opts = function(_, opts)
+  --     local x = opts.sections.lualine_x
+  --     for _, comp in ipairs(x) do
+  --       if comp[1] == "diff" then
+  --         comp.source = function()
+  --           local summary = vim.b.minidiff_summary
+  --           return summary
+  --             and {
+  --               added = summary.add,
+  --               modified = summary.change,
+  --               removed = summary.delete,
+  --             }
+  --         end
+  --         break
+  --       end
+  --     end
+  --   end,
+  -- },
 }
