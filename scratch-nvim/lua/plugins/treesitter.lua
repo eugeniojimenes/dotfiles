c455fba04ff0be77610c5cd1434f41b576b4785b
Eugenio Augusto Jimenes <eugeniojimenes@gmail.com>
2025-06-21 19:02:48 -0300

    feat(scratch-nvim): clean up

--- scratch-nvim/lua/plugins/treesitter.lua
+++ scratch-nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,6 @@ return {
     opts = {
       highlight = { enable = true },
       indent = { enable = true },
-      folding = { enable = true, custom_foldtext = true },
       ensure_installed = {
         "bash",
         "diff",
@@ -31,24 +30,6 @@ return {
         "xml",
         "yaml",
       },
-      -- incremental_selection = {
-      --   enable = true,
-      --   keymaps = {
-      --     init_selection = "<C-space>",
-      --     node_incremental = "<C-space>",
-      --     scope_incremental = false,
-      --     node_decremental = "<bs>",
-      --   },
-      -- },
-      -- textobjects = {
-      --   move = {
-      --     enable = true,
-      --     goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
-      --     goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
-      --     goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
-      --     goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
-      --   },
-      -- },
     },
   },
 }
