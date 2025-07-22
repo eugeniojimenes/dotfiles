3539c1a2b9e9efb0f5fb581e6ca7831d5a9d5f9b
Eugenio Augusto Jimenes <eugeniojimenes@gmail.com>
2025-07-22 19:06:23 -0300

    feat: mason-lsp + git fugitive

--- scratch-nvim/lua/plugins/mason.lua
+++ scratch-nvim/lua/plugins/mason.lua
@@ -3,4 +3,21 @@ return {
     "mason-org/mason.nvim",
     opts = {},
   },
+  {
+      "mason-org/mason-lspconfig.nvim",
+      dependencies = {
+          { "mason-org/mason.nvim", opts = {} },
+          "neovim/nvim-lspconfig",
+      },
+      opts = {
+        ensure_installed = {
+          "lua_ls",
+          "solargraph",
+          -- "vtsls",
+          "ts_ls",
+          "ruff",
+          "pyright",
+        },
+      },
+  }
 }
