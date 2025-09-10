34b580072902a836237e17a1150f57efb8918f13
Eugenio Augusto Jimenes <eugeniojimenes@gmail.com>
2025-09-10 10:54:11 -0300

    feat(tmux): remove third window from tmux-start.sh script

--- tmux/tmux-start.sh
+++ tmux/tmux-start.sh
@@ -2,6 +2,5 @@
 tmux -L start has-session -t start 2>/dev/null && tmux -L start attach -t start || {
     tmux -L start new-session -d -s start -n btop bash -c 'btop; exec bash';
     tmux -L start new-window -d -t start: -n lazydocker bash -c 'lazydocker; exec bash';
-    tmux -L start new-window -d -t start: -n new-window;
     tmux -L start attach -t start;
 }
