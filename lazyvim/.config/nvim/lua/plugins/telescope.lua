8f63b36ebbeca2eb682855261c75daff8f86be2a
Eugenio Augusto Jimenes <eugeniojimenes@gmail.com>
2025-10-21 19:20:54 -0300

    feat: lazyvim and omarchy-update

---
+++ lazyvim/.config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      defaults = {
+        layout_config = {
+          -- vertical = { width = 0.98 },
+          horizontal = { width = 0.98 }
+        },
+      },
+    },
+  },
+}
