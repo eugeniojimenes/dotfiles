f0a1b907a552ea77760a0b993f751c3cde3ecae4
Eugenio Augusto Jimenes <eugeniojimenes@gmail.com>
2025-11-03 17:11:00 -0300

    feat(lazyvim): increase minimal legth to snacks truncate the file path

--- bash/.bash_profile
+++ bash/.bash_profile
@@ -1,3 +1,3 @@
 [[ -f ~/.bashrc ]] && . ~/.bashrc
-source ~/my-zk/envs
+source ~/my-zk/envs # INFO: replace with your own path of private envs
 export ZK_NOTEBOOK_DIR="$HOME/my-zk"
--- lazyvim/.config/nvim/lua/plugins/snacks.lua
+++ lazyvim/.config/nvim/lua/plugins/snacks.lua
@@ -3,6 +3,12 @@ return {
   opts = {
     picker = {
       layout = { preset = "default" }, -- use the telescope layout preset
+      formatters = {
+        file = {
+          truncate = "left",
+          min_width = 60, -- minimum length of the truncated path
+        }
+      }
     },
   },
 }
