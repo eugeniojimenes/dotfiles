7e89f5a614010da6249a8f96c0af08711e49601d
Eugenio Augusto Jimenes <eugeniojimenes@gmail.com>
2025-12-05 18:32:42 -0300

    feat: remove nx and add lsp ensure_install

---
+++ lazyvim/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "debugpy",
+        "erb-formatter",
+        "erb-lint",
+        "eslint-lsp",
+        "js-debug-adapter",
+        "json-lsp",
+        "lua-language-server",
+        "prettier",
+        "prisma-language-server",
+        "pyright",
+        "rubocop",
+        "ruff",
+        "shfmt",
+        "solargraph",
+        "stylua",
+        "taplo",
+        "vtsls",
+      },
+    },
+  },
+}
